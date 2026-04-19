793757bef05ab91b799f2158902576cef25c5ecc
Salah Saleh <salahsaleh1015@gmail.com>
2026-04-19 20:13:50 +0200

    my cart view finish

---
+++ lib/features/checkout/presentation/views/my_cart.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/widgets/cutom_app_bar.dart';
+import '../widgets/my_cart_view_body.dart';
+
+class MyCartView extends StatelessWidget {
+  const MyCartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(title: 'My Cart'),
+      body: const MyCartViewBody(),
+    );
+  }
+}
---
+++ lib/features/checkout/presentation/widgets/my_cart_view_body.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:gateway_payments_app/features/checkout/presentation/widgets/cart_info_item.dart';
+import 'package:gateway_payments_app/features/checkout/presentation/widgets/total_price_widget.dart';
+
+import '../../../../core/widgets/custom_button.dart';
+
+class MyCartViewBody extends StatelessWidget {
+  const MyCartViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 18,
+          ),
+          Expanded(child: Image.asset('assets/images/basket_image.png',fit: BoxFit.fill,)),
+          const SizedBox(
+            height: 40,
+          ),
+          const OrderInfoItem(
+            title: 'Order Subtotal',
+            value: r'42.97$',
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(
+            title: 'Discount',
+            value: r'0$',
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(
+            title: 'Shipping',
+            value: r'8$',
+          ),
+          const Divider(
+            thickness: 2,
+            height: 34,
+            color: Color(0xffC7C7C7),
+          ),
+          const TotalPrice(title: 'Total', value: r'$50.97'),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomButton(
+            text: 'Complete Payment',
+            onTap: () {},
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+        ],
+      ),
+    );
+  }
+}
