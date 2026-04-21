522286e04e9c3ff80a68b8190e0994f82e214f96
Salah Saleh <salahsaleh1015@gmail.com>
2026-04-21 21:23:08 +0200

    payment details part 2  validate user input

--- lib/features/checkout/presentation/widgets/payment_details_view_body.dart
+++ lib/features/checkout/presentation/widgets/payment_details_view_body.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:gateway_payments_app/features/checkout/presentation/widgets/custom_credit_card.dart';
 import 'package:gateway_payments_app/features/checkout/presentation/widgets/payment_methods_list_view.dart';
+
+import '../../../../core/widgets/custom_button.dart';
 
 class PaymentDetailsViewBody extends StatefulWidget {
   const PaymentDetailsViewBody({super.key});
@@ -11,7 +15,7 @@ class PaymentDetailsViewBody extends StatefulWidget {
 
 class _PaymentDetailsViewBodyState extends State<PaymentDetailsViewBody> {
   final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +26,33 @@ class _PaymentDetailsViewBodyState extends State<PaymentDetailsViewBody> {
         ),
         SliverToBoxAdapter(
           child: CustomCreditCard(
-            autovalidateMode: autovalidateMode,
+            autovalidateMode: autoValidateMode,
             formKey: formKey,
           ),
+        ),
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 12, left: 16, right: 16),
+                child: CustomButton(
+                  onTap: () {
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState!.save();
+                      log('payment');
+                    } else {
+                      // Navigator.of(context)
+                      //     .push(MaterialPageRoute(builder: (context) {
+                      //   return const ThankYouView();
+                      // }));
+                      autoValidateMode = AutovalidateMode.always;
+                      setState(() {});
+                    }
+                  },
+                  text: 'Payment',
+                ),
+              )),
         ),
       ],
     );
